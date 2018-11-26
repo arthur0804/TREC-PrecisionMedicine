750974dffa59ba063ecc22b78437afdc11fb8f2b
Jiaming Qu <jiaming@ad.unc.edu>
2018-11-26 00:41:39 -0500

    update on Nov.26

--- src/BooleanRetrieval.java
+++ src/BooleanRetrieval.java
@@ -12,96 +12,82 @@ import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader; 
 import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.index.Term;
 import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
 import org.apache.lucene.queryparser.classic.ParseException; 
 import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.queryparser.classic.QueryParser.Operator;
-import org.apache.lucene.search.BooleanClause;
-import org.apache.lucene.search.BooleanClause.Occur;
-import org.apache.lucene.search.BooleanQuery;
 import org.apache.lucene.search.IndexSearcher; 
 import org.apache.lucene.search.Query; 
-import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.search.TermQuery;
+import org.apache.lucene.search.ScoreDoc; 
 import org.apache.lucene.search.TopDocs ;
+import org.apache.lucene.search.similarities.BM25Similarity;
 import org.apache.lucene.store.Directory; 
 import org.apache.lucene.store.FSDirectory;
 
-
 public class BooleanRetrieval {
 	public static void SearchMethod(ArrayList<String> queries) throws ParseException, IOException {
-
+		
+		// search for the CONTENT field (abstract text) and Title field , which is defined in CreateIndex
+		String[] fields ={"title", "content"};
+		
+		// boosts
+		// Map<String, Float> boosts = new  HashMap();
+		// boosts.put("title", (float) 1.5);
+		// boosts.put("content", (float) 0.5);
+		
 		// set directory of indexes
 		Path indexPath = Paths.get("/proj/wangyue/jiamingfolder/index");
 		Directory dir = FSDirectory.open(indexPath);
 				
-		// create index reader, writer, analyzer, query parser
+		// create index reader
 		IndexReader reader = DirectoryReader.open(dir);
+		
+		// create index searcher and set BM25 similarity
 		IndexSearcher searcher = new IndexSearcher(reader);
 		
+		// set BM25 model
+		// searcher.setSimilarity(new BM25Similarity());
+		
+		// create analyzer
+		Analyzer analyzer = new StandardAnalyzer();
+		
+		// create query parser
+		MultiFieldQueryParser parser = new MultiFieldQueryParser (fields, analyzer);
+		parser.setDefaultOperator(Operator.AND);
 		
 		// execute queries and write the result into a text file			
 		// create headers in the result log
 		String header = "TOPIC_NO" + " " + " Q0" + " " + "ID" + " " + "RANK" + " " + "SCORE" + " " + "RUN_NAME" + "\n";
-		Files.write(Paths.get("/proj/wangyue/jiamingfolder/dat/boolean_retrieval.txt"), header.getBytes(), StandardOpenOption.APPEND);
-			
+		Files.write(Paths.get("/proj/wangyue/jiamingfolder/dat/basic_query_result_withboost_2.txt"), header.getBytes(), StandardOpenOption.APPEND);
+		
+		
 		// iterate through the queries list to execute
 		int topic_no = 1;
 		for(String query : queries) {
-			String[] SplittedQueryTerms = query.split("\\s+");
-			Query[] QuerryTermArray = new Query[SplittedQueryTerms.length];
-			BooleanClause[] booleanClauses = new BooleanClause[SplittedQueryTerms.length];
+			Query multifieldquery = parser.parse(query);
 			
-			for(int i = 0; i < SplittedQueryTerms.length; i++) {
-				QuerryTermArray[i] = new TermQuery (new Term("content",  SplittedQueryTerms[i]));
-				booleanClauses[i] = new BooleanClause (QuerryTermArray[i], Occur.MUST);
+			// top 1000 results
+			TopDocs tds = searcher.search(multifieldquery, 1000);
+			// document rank in the retrieval result
+			int rank = 1; 
+			
+			for(ScoreDoc sd : tds.scoreDocs) {		
+				Document document = searcher.doc(sd.doc);
+						
+				String TOPIC_NO = String.valueOf(topic_no);
+				String Q0 = "0";
+				String ID = document.get("id");
+				String RANK = String.valueOf(rank);
+				String SCORE = String.valueOf(sd.score);
+				String RUN_NAME = "my_run";
+				String NEW_RECORD = TOPIC_NO + " " + Q0 + " " + ID + " " + RANK + " " + SCORE + " " + RUN_NAME + "\n";
+				Files.write(Paths.get("/proj/wangyue/jiamingfolder/dat/basic_query_result_withboost_2.txt"), NEW_RECORD.getBytes(), StandardOpenOption.APPEND);
+						
+				rank ++;
+				// end of the loop for 1k documents
 			}
-			
-			switch (booleanClauses.length) {
-			case 1:
-				BooleanQuery boolQuery_1=new BooleanQuery.Builder().add(booleanClauses[0]).build();
-				TopDocs tds_1 = searcher.search(boolQuery_1, 1000);
-				rankDocuments (searcher, tds_1,topic_no);
-				break;
-			case 2:
-				BooleanQuery boolQuery_2=new BooleanQuery.Builder().add(booleanClauses[0]).add(booleanClauses[1]).build();
-				TopDocs tds_2 = searcher.search(boolQuery_2, 1000);
-				rankDocuments (searcher, tds_2,topic_no);
-				break;
-			case 3:
-				BooleanQuery boolQuery_3=new BooleanQuery.Builder().add(booleanClauses[0]).add(booleanClauses[1]).add(booleanClauses[2]).build();
-				TopDocs tds_3 = searcher.search(boolQuery_3, 1000);
-				rankDocuments (searcher, tds_3,topic_no);
-				break;
-			default:
-				BooleanQuery boolQuery_4=new BooleanQuery.Builder().add(booleanClauses[0]).add(booleanClauses[1]).add(booleanClauses[2]).add(booleanClauses[3]).build();
-				TopDocs tds_4 = searcher.search(boolQuery_4, 1000);
-				rankDocuments (searcher, tds_4,topic_no);
-				break;
-			}			
 		topic_no ++ ;
 		// end of the loop for 30 queries
 		}	
-	}
-	public static void rankDocuments (IndexSearcher searcher, TopDocs tds,int topic_no) throws IOException {
-			// document rank in the retrieval result
-			int rank = 1; 
-					
-			for(ScoreDoc sd : tds.scoreDocs) {		
-					Document document = searcher.doc(sd.doc);
-								
-					String TOPIC_NO = String.valueOf(topic_no);
-					String Q0 = "0";
-					String ID = document.get("id");
-					String RANK = String.valueOf(rank);
-					String SCORE = String.valueOf(sd.score);
-					String RUN_NAME = "my_run";
-					String NEW_RECORD = TOPIC_NO + " " + Q0 + " " + ID + " " + RANK + " " + SCORE + " " + RUN_NAME + "\n";
-					Files.write(Paths.get("/proj/wangyue/jiamingfolder/dat/boolean_retrieval.txt"), NEW_RECORD.getBytes(), StandardOpenOption.APPEND);
-								
-					rank ++;
-					// end of the loop for 1k documents
-			}
 	}
 }
